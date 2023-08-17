a4647344193e507ae5dbaa4834928effdcb43579
Steven H <smhanson21@gmail.com>
2023-08-17 17:12:57 -0500

    add questions tab with github username and email address

--- Develop/DYNAMIC_README.md
+++ Develop/DYNAMIC_README.md
@@ -1,4 +1,6 @@
 # Title
+![Static badge](https://img.shields.io/badge/Unlicense-violet) Click [here](https://choosealicense.com/licenses/unlicense) for more info on the license.
+
 ## Description
 Description
 
@@ -12,24 +14,26 @@ Description
 * [Tests](#Tests)
 
 ## Installation
-lsiukevtmyslkdrtv hsldekrtv hsdrlktv mdhslktv jhsdvflgkjhsdvlkjah nv;gahv sdkjhva s;kdvj h;kasd hg;kha s;kdjhg a;ksjhd ;akjh dsf;kjhasdfl kjhaskldj fhkljhfiuewh fiawuehf iouwhefoiuhwaeoifluhwiolfh oiawhef iawhefoiahwfoiuhwaeofih awoieufhwaiefhpauehfpaso;idufhpaoisudhvnipaweunfpaidsunv paieuf napiouef npasoiduvn poa;uien foasudfhi oasdjhf poaiwejfoaiejfoinaon; ij; n; a; l ;n dflka slnfkhas dflknhas dflkhasd fkljhasd flkhas dfnlkjasd fiou iouysd foiuydsf giouyr lkhjerw bjka likhjsdf yoxzvcpoaywerkljbnawer98sydv0976234lkjbnawer90768sdf kl23jn4 v2349087nv23904v8n0938 n70q293 8709 209 hn092  98209 83 209 
+Installation
 
 ## Usage
-9v 8n40v9837v5098n50n93vq457nv4098vn0947vn03948vn7309v5n7w039v4n857093w4nv5809wn3v485vwn09345n039w4v5nw3094vn53094vn53094v8n53w4vn95038w4v75n093w4v5n7930w8vn5093w7vn5w03v5n7w03948vn5703w9nv573w094n8v570w39v7n5w039nv57w309v5n8734v5n903w85v7n0w395nv7w8v5n0w35vn03w49n5v03wn5v0w3nv5w3054nv9w3v054n98w3v4509n87w34v5098n7w345v098n7w34v509n87w34v5098n7w34v50987nw34v50987nw345v0987nw345v0987nw345v0987nw345v0987nw345v0987nw345v0987nw345v0987nw345v0987nw345v0987nw34v50987nw34v50987nw345v0987nw345v098n7w345v0987nw345v098n7w345v0987nw345v0987nw345v0987nw345v0987nw345v0987nw345v0987w345v0987nw345v0987nw345v0987nw345v]
+Use it
 
 ## Credits
-Nope
+Credits
 
 ## License
-Mozilaa Public License 2.0
-
-![Static badge](https://img.shields.io/badge/) Click [here](https://choosealicense.com/licenses/) for more info on the license.
+This project is licensed under The Unlicense
+See badge for more info.
 
 ## Features
-ase fa ae faw efopawie uvrapwoeriumcawpeorcuawercaweocriuawecrpouacer   
+Features   
 
 ## How-to-Contribute
-Contriburte
+Devs
 
 ## Tests
 Test
+
+## Questions
+For questions, reach out to them on [GitHub](https://github.com/hansonsteven26), or contact them [here](mailto:smhanson21@gmail.com)
--- Develop/index.js
+++ Develop/index.js
@@ -4,63 +4,75 @@ const generateBadge = require('./utils/generateMarkdown.js')
 
 function init() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "Please leave any question blank if it is not applicable\nWhat is the title of your project?",
-            name: "projectName"
-        },
-        {
-            type: "input",
-            message: "What is your project's description?",
-            name: "projectDesc"
-        },
-        {
-            type: "input",
-            message: "What are the steps to install your project?",
-            name: "projectInstall"
-        },
-        {
-            type: "input",
-            message: "How do you use your project?",
-            name: "projectUsage"
-        },
-        {
-            type: "input",
-            message: "Include any credits that you used in your project",
-            name: "projectCredits"
-        },
-        {
-            type: "list",
-            message: "Which license does your project want?",
-            choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilaa Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"],
-            name: "projectLicense"
-        },
-        {
-            type: "input",
-            message: "List your project's features if there are a lot",
-            name: "projectFeatures"
-        },
-        {
-            type: "input",
-            message: "Let other developers know how they can contribute to your project",
-            name: "projectContribute"
-        },
-        {
-            type: "input",
-            message: "Write tests for your project",
-            name: "projectTests"
-        },
-        {
-            type: "input",
-            message: "Include a table of contents? (Leave blank if no)",
-            name: "projectToC"
-        },
-    ])
-    .then(response => {
-        let badge = generateBadge(response.projectLicense);
-        let readMeTemplate =     
-`# ${response.projectName}
+        .prompt([
+            {
+                type: "input",
+                message: "Please leave any question blank if it is not applicable\nWhat is the title of your project?",
+                name: "projectName"
+            },
+            {
+                type: "input",
+                message: "What is your project's description?",
+                name: "projectDesc"
+            },
+            {
+                type: "input",
+                message: "What are the steps to install your project?",
+                name: "projectInstall"
+            },
+            {
+                type: "input",
+                message: "How do you use your project?",
+                name: "projectUsage"
+            },
+            {
+                type: "input",
+                message: "Include any credits that you used in your project",
+                name: "projectCredits"
+            },
+            {
+                type: "list",
+                message: "Which license does your project want?",
+                choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilaa Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"],
+                name: "projectLicense"
+            },
+            {
+                type: "input",
+                message: "List your project's features if there are many",
+                name: "projectFeatures"
+            },
+            {
+                type: "input",
+                message: "Let other developers know how they can contribute to your project",
+                name: "projectContribute"
+            },
+            {
+                type: "input",
+                message: "Write tests for your project",
+                name: "projectTests"
+            },
+            {
+                type: "input",
+                message: "Include a table of contents? (Leave blank if no)",
+                name: "projectToC"
+            },
+            {
+                type: "input",
+                message: "What is your GitHub username?",
+                name: "projectUserName"
+            },
+            {
+                type: "input",
+                message: "What is your email address? (So people can ask you questions)",
+                name: "projectEmail"
+            }
+        ])
+        .then(response => {
+            let badge = generateBadge(response.projectLicense);
+            let readMeTemplate =
+                `# ${response.projectName}
+${badge}
+
 ## Description
 ${response.projectDesc}
 
@@ -76,8 +88,8 @@ ${response.projectCredits ? '## Credits' : ''}
 ${response.projectCredits}
 
 ${response.projectLicense ? '## License' : ''}
-${response.projectLicense}\n
-${badge}
+This project is licensed under ${response.projectLicense}
+See badge for more info.
 
 ${response.projectFeatures ? '## Features' : ''}
 ${response.projectFeatures}   
@@ -87,12 +99,15 @@ ${response.projectContribute}
 
 ${response.projectTests ? '## Tests' : ''}
 ${response.projectTests}
-`
-        fs.writeFile("DYNAMIC_README.md", readMeTemplate, err => {
-            err ? console.error(err) : console.log('Code worked!')
-          })
 
-    })
+## Questions
+For questions, reach out to them on [GitHub](https://github.com/${response.projectUserName}), or contact them [here](mailto:${response.projectEmail})
+`
+            fs.writeFile("DYNAMIC_README.md", readMeTemplate, err => {
+                err ? console.error(err) : console.log('Code worked!')
+            })
+
+        })
 }
 
 init();
